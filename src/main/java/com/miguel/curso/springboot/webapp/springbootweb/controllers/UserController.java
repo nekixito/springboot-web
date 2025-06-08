38fbb8c90caacaff77e53fe0166a9c444b5be021
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-06-08 00:08:00 -0600

    Hola Mundo Spring Boot

---
+++ src/main/java/com/miguel/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.miguel.curso.springboot.webapp.springbootweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/details")
+    public String details(){
+        return "details";
+    }
+}
