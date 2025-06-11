075a0154680ec75918ad1b3e71b290cc47cb6ce2
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-06-11 13:25:06 -0600

    Pasar datos a la vista con Model y Map

--- src/main/java/com/miguel/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/miguel/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,13 +1,30 @@
 package com.miguel.curso.springboot.webapp.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
-        return "details";
+    public String details(Model model){
+        model.addAttribute("title","Hola Mundo Spring Boot");
+        model.addAttribute("name","Miguel");
+        model.addAttribute("lastname","Nequiz");
+
+       return "details";
     }
+
+    /* Otra forma
+    @GetMapping("/details")
+    public String details(Map<String, Object> model){
+        model.put("title","Hola Mundo Spring Boot");
+        model.put("name","Miguel");
+        model.put("lastname","Nequiz");
+
+       return "details";
+    } */
 }
